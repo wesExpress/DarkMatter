c6d771676207e17f53933cb70ab4939a2bbebc02
Wesley Peters <wespeters18@gmail.com>
2022-01-25 10:05:34 -0500

    comments for list functions

--- src/structures/dm_list.h
+++ src/structures/dm_list.h
@@ -16,13 +16,35 @@ typedef struct dm_list
 dm_list* dm_list_create(size_t element_size, size_t capacity);
 void dm_list_destroy(dm_list* list);
 
+/*
+append a value to end of list
+*/
 void dm_list_append(dm_list* list, void* value);
+
+/*
+insert a value into list at specified index
+*/
 void dm_list_insert(dm_list* list, void* value, uint32_t index);
 
+/*
+delete an element from end of list
+*/
 void dm_list_pop(dm_list* list);
+
+/*
+delete an element from list at a specified index
+*/
 void dm_list_pop_at(dm_list* list, uint32_t index);
 
+/*
+returns value from a list at specified index.
+It is the user's responsibility to cast this to whatever it actually is!
+*/
 void* dm_list_at(dm_list* list, uint32_t index);
+
+/*
+clears a list and resets capacity to specified amount. If 0, then 16 elements
+*/
 void dm_list_clear(dm_list* list, size_t new_capacity);
 
 bool dm_list_is_empty(dm_list* list);
