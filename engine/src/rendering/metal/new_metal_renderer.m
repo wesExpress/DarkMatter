ee5cd57825153d681f8e45c5964f2612a281f63e
Wesley Peters <wespeters18@gmail.com>
2022-04-28 15:28:05 -0400

    apparently you need to set the buffer data to the fragment shader as wellgit add *! It's working, even though things are messed up, stuff is being drawn!

--- engine/src/rendering/metal/new_metal_renderer.m
+++ engine/src/rendering/metal/new_metal_renderer.m
@@ -621,6 +621,7 @@ bool dm_renderer_bind_uniforms_impl(uint32_t slot, dm_render_pass* render_pass)
 		//dm_memcpy([internal_pass->uniform_buffer contents], buffer_data, aligned_size);
 		internal_pass->uniform_buffer = [renderer.device newBufferWithBytes:buffer_data length:aligned_size options:MTLResourceOptionCPUCacheModeDefault];
 		[renderer.command_encoder setVertexBuffer:internal_pass->uniform_buffer offset:0 atIndex:slot];
+		[renderer.command_encoder setFragmentBuffer:internal_pass->uniform_buffer offset:0 atIndex:0];
 	}
 
 	return true;
